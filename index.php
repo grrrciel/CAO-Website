ae7b4ac245daa69ab289e4e68ad9cac8b7ae4f2d
grrrciel7 <grrrciel@gmail.com>
2025-05-01 18:47:53 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,433 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="DJoz Template">
+    <meta name="keywords" content="DJoz, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="icon/png" href="img/last.png">
+    <link rel="stylesheet" href="./style.css">
+    <title>Cultural Affairs Office</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/barfiller.css" type="text/css">
+    <link rel="stylesheet" href="css/nowfont.css" type="text/css">
+    <link rel="stylesheet" href="css/rockville.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+        <img src="img/loader.gif" alt="">
+    </div>
+
+<?php include 'header.php'; ?>
+
+
+    <!-- Header Section End -->
+
+
+    <br><br><br><br><br><br><br><br><br><br><br>
+    <!-- Hero Section Begin -->
+    <div class="banner">
+        <video autoplay loop muted playsinline>
+            <source src="img/1.mp4" type="video/mp4">
+        </video>
+        <div class="grid-overlay"></div>
+    </div>    
+        <div class="ccontainer">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="hero__text">
+                        <span>Welcome to</span>
+                        <h1>Cultural Affairs Office</h1>
+                        <p style="text-align: center;">#SievingForExcellence</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Hero Section End -->
+    <br><br><br><br><br><br>
+    <!-- Event Section Begin -->
+    <section class="event spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title">
+                        <h2>Upcoming Events</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="event__slider owl-carousel">
+                    <div class="col-lg-4">
+                        <div class="event__item">
+                            <div class="event__item__pic set-bg" data-setbg="img/events/event-1.jpg">
+                                <div class="tag-date">
+                                    <span>February 27, 2025</span>
+                                </div>
+                            </div>
+                            <div class="event__item__text">
+                                <h4>Samot-Sari 2025</h4>
+                                <p><i class="fa fa-map-marker"></i> CLSU Auditorium</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="event__item">
+                            <div class="event__item__pic set-bg" data-setbg="img/events/event-2.jpg">
+                                <div class="tag-date">
+                                    <span>March 05, 2025</span>
+                                </div>
+                            </div>
+                            <div class="event__item__text">
+                                <h4>HipHop Auditions</h4>
+                                <p><i class="fa fa-map-marker"></i> CLSU Gymnatorium</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="event__item">
+                            <div class="event__item__pic set-bg" data-setbg="img/events/event-3.jpg">
+                                <div class="tag-date">
+                                    <span>February 15, 2025</span>
+                                </div>
+                            </div>
+                            <div class="event__item__text">
+                                <h4>National Arts Month</h4>
+                                <p><i class="fa fa-map-marker"></i> FPJ Hall</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="event__item">
+                            <div class="event__item__pic set-bg" data-setbg="img/events/event-2.jpg">
+                                <div class="tag-date">
+                                    <span>March 05, 2025</span>
+                                </div>
+                            </div>
+                            <div class="event__item__text">
+                                <h4>HipHop Auditions</h4>
+                                <p><i class="fa fa-map-marker"></i> CLSU Gymnatorium</p>
+                            </div>
+                        </div>
+                    </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Event Section End -->
+
+    <!-- About Section Begin -->
+    <section class="about spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="about__pic">
+                        <img src="img/about.png" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="about__text">
+                        <div class="section-title">
+                            <h2>Cultural Affairs Office</h2>
+                        </div>
+                        <p>CLSU Cultural Affairs Office (CAO) provides production and presentation venues for various local and international shows and performances it unites the different performing groups, envisioned to offer an array of skillful performing arts programs and events to the CLSU community and other interested parties.</p>
+                        <a href="img/Request Performance PDF.pdf" download class="primary-btn">DOWNLOAD PERFORMANCE REQUEST FORM</a>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- About Section End -->
+
+
+    <!-- Youtube Section Begin -->
+    <section class="youtube spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title">
+                        <h2>Youtube feed</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="youtube__item">
+                        <div class="youtube__item__pic set-bg" data-setbg="img/youtube/youtube-1.jpg">
+                            <a href="https://www.youtube.com/watch?v=LagmzRpW9GE&ab_channel=CLSUMaestroSingers" target="_blank" class="play-btn">
+                                <i class="fa fa-play"></i>
+                            </a>
+                        </div> 
+                        <div class="youtube__item__text">
+                            <h4>Magdiwang Magpuri - CLSU Maestro Singers</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="youtube__item">
+                        <div class="youtube__item__pic set-bg" data-setbg="img/youtube/youtube-2.jpg">
+                            <a href="https://www.youtube.com/watch?v=BncDPtAedMI" target="_blank" class="play-btn">
+                                <i class="fa fa-play"></i>
+                            </a>
+                        </div>                  
+                        <div class="youtube__item__text">
+                            <h4>Dance Performances - Tanghalang Gagalaw sa CLSU</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="youtube__item">
+                        <div class="youtube__item__pic set-bg" data-setbg="img/youtube/youtube-3.jpg">
+                            <a href="https://www.youtube.com/watch?v=6TH4zngjvMw&ab_channel=IsaganiBotabara" target="_blank" class="play-btn">
+                                <i class="fa fa-play"></i>
+                            </a>
+                        </div>   
+                        <div class="youtube__item__text">
+                            <h4>Sayaw Pinoy(NCR) - CLSU Bithay Sining Dance Company</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Youtube Section End -->
+
+    <!-- Countdown Section Begin -->
+    <section class="countdown spad set-bg" data-setbg="img/index/countdown.png" style="min-height: 100vh;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="countdown__text">
+                        <h1>Samot-Sari 2025</h1>
+                        <h5 style="color: white; font-family: 'Acumin Pro';">CULTURAL SHOW WILL START IN</h5>
+                    </div>
+                    <div class="countdown__timer" id="countdown-time">
+                        <div class="countdown__item">
+                            <span>7</span>
+                            <p>days</p>
+                        </div>
+                        <div class="countdown__item">
+                            <span>24</span>
+                            <p>hours</p>
+                        </div>
+                        <div class="countdown__item">
+                            <span>18</span>
+                            <p>minutes</p>
+                        </div>
+                        <div class="countdown__item">
+                            <span>09</span>
+                            <p>seconds</p>
+                        </div>
+                    </div>
+                    <div class="buy__tickets">
+<!-- Watch Trailer Button -->
+<a href="#" class="primary-btn" onclick="openVideoPopup()">Watch Trailer</a>
+
+<!-- Video Popup Modal -->
+<div id="videoModal" class="modal">
+    <div class="modal-content">
+        <span class="close" onclick="closeVideoPopup()">&times;</span>
+        <video id="trailerVideo" controls>
+            <source src="img/trailer.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+    </div>
+</div>
+
+<!-- CSS for Styling -->
+<style>
+    .modal {
+        display: none;
+        position: fixed;
+        z-index: 1000;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.8);
+        justify-content: center;
+        align-items: center;
+    }
+
+    .modal-content {
+        position: relative;
+        background: #000000;
+        padding: 10px;
+        width: 80%;
+        max-width: 700px;
+        text-align: center;
+        border-radius: 8px;
+    }
+
+/* Video Popup Container */
+/* Hide video by default */
+#videoPopup {
+    display: none;
+    position: fixed;
+    z-index: 1000;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 80%;
+    max-width: 700px;
+    background: black;
+    border-radius: 8px;
+    overflow: hidden;
+}
+
+/* Popup content */
+.popup-content {
+    position: relative;
+    background: black;
+    padding: 10px;
+    width: 100%;
+    text-align: center;
+    border-radius: 8px;
+}
+
+/* Close button (Properly Positioned at the top-right corner of the container) */
+.close {
+    position: absolute;
+    top: 10px;
+    right: 15px;
+    font-size: 24px;
+    cursor: pointer;
+    color: rgb(0, 0, 0); /* Ensure visibility */
+    z-index: 1001; /* Ensure it's above other elements */
+    padding: 0px 0px;
+}
+
+/* Popup Video Container */
+.popup {
+    display: none;
+    position: fixed;
+    z-index: 1000; /* Behind the close button */
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 80%;
+    max-width: 700px;
+    background: black;
+    border-radius: 8px;
+    overflow: hidden;
+}
+
+/* Video Styling */
+.popup video {
+    display: block;
+    width: 100%;
+    border-radius: 8px;
+}
+
+</style>
+
+<!-- JavaScript for Opening and Closing Modal -->
+<script>
+    function openVideoPopup() {
+        document.getElementById('videoModal').style.display = 'flex';
+        document.getElementById('trailerVideo').play();
+    }
+
+    function closeVideoPopup() {
+        document.getElementById('videoModal').style.display = 'none';
+        document.getElementById('trailerVideo').pause();
+    }
+</script>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Countdown Section End -->
+
+<!-- BACK TO TOP BUTTON -->
+<!-- <a href="#" class="back-to-top">
+    <span class="material-icons">↑</span>
+</a> -->
+
+<div id="scrollUp" class="orange-color">
+    <i class="fa fa-angle-up"></i>
+</div>
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        let scrollUp = document.getElementById("scrollUp");
+
+        window.addEventListener("scroll", function () {
+            if (window.scrollY > 200) {
+                scrollUp.style.display = "flex"; // Show button when scrolling down
+            } else {
+                scrollUp.style.display = "none"; // Hide button when at the top
+            }
+        });
+
+        // Smooth scroll to top when clicked
+        scrollUp.addEventListener("click", function () {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        });
+    });
+</script>
+
+<!-- Footer Section Begin --><br>
+<footer class="footer spad set-bg" data-setbg="img/footer-bg.png" style="text-align: center;">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6">
+                <div class="footer__social"><br>
+                    <h2 style="white-space: nowrap;">Cultural Affairs Office</h2>
+                    <div class="footer__social__links" style="display: flex; justify-content: center; gap: 15px; margin-top: 10px;">
+                        <a href="https://www.facebook.com/CulturalAffairsOfficeCLSU" target="_blank">
+                            <i class="fa fa-facebook"></i>
+                        </a>
+                        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=clsu.cao@clsu.edu.ph" target="_blank">
+                            <i class="fa fa-envelope"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Copyright Text -->
+        <div class="footer__copyright__text" style="display: flex; justify-content: center; align-items: center; text-align: center; width: 100%; margin-top: 10px;">
+            <p>Copyright © 2025 | Central Luzon State University - Cultural Affairs Office | All Rights Reserved</p>
+        </div>
+    </div>
+</footer>
+<!-- Footer Section End -->
+
+
+    <!-- Js Plugins -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.nicescroll.min.js"></script>
+    <script src="js/jquery.barfiller.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+
+    <!-- Music Plugin -->
+    <script src="js/jquery.jplayer.min.js"></script>
+    <script src="js/jplayerInit.js"></script>
+</body>
+
+</html>
